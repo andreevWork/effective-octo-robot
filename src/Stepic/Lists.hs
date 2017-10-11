77184143870beca5a3079015354efedadc151e4c
vandreev <vandreevwork94@gmail.com>
2017-10-11 15:44:53 +0300

    add stepics lists tasks

--- src/Stepic/Lists.hs
+++ src/Stepic/Lists.hs
@@ -1,7 +1,9 @@
 module Stepic.Lists where
 
+import Data.List
 
--- 3.1 Функции для работы со списками. 10 задание
+
+-- 3.1 Функции для работы со списками
 -- https://stepik.org/lesson/8326/step/10?unit=1474
 isPalindrome :: Eq a => [a] -> Bool
 
@@ -10,3 +12,16 @@ isPalindrome [] = True
 isPalindrome xs
     | head xs == last xs = isPalindrome $ init . tail $ xs
     | otherwise = False
+
+-- https://stepik.org/lesson/8326/step/12?unit=1474
+sum3 :: Num a => [a] -> [a] -> [a] -> [a]
+
+sum3 a b c = map sum $ transpose [a, b, c]
+
+-- https://stepik.org/lesson/8326/step/13?unit=1474
+groupElems :: Eq a => [a] -> [[a]]
+
+groupElems [] = []
+groupElems xs@(x:_) = f : groupElems s
+    where
+        (f, s) =  span (== x) xs
--- test/Stepic/ListsSpec.hs
+++ test/Stepic/ListsSpec.hs
@@ -4,18 +4,33 @@ import Test.Hspec
 import Stepic.Lists
 
 spec :: Spec
-spec = do
+spec = describe "Stepic" $ do
+        describe "isPalindrome" $ do
            it "simple" $ do
              isPalindrome [1,3,3,1] `shouldBe` True
 
-           it "simple" $ do
+           it "one" $ do
              isPalindrome [1] `shouldBe` True
 
-           it "simple" $ do
+           it "string" $ do
              isPalindrome "asdffdsa" `shouldBe` True
 
-           it "simple" $ do
+           it "middle" $ do
              isPalindrome "asdf1fdsa" `shouldBe` True
 
-           it "simple" $ do
+           it "false" $ do
              isPalindrome [1,34] `shouldBe` False
+
+        describe "sum3" $ do
+           it "simple" $ do
+             sum3 [1,2,3] [4,5] [6] `shouldBe` [11,7,3]
+
+           it "simple 2" $ do
+             sum3 [1] [4,5,5,6] [0, 6] `shouldBe` [5,11,5,6]
+
+        describe "groupElems" $ do
+           it "simple" $ do
+             groupElems [1,2] `shouldBe` [[1],[2]]
+
+           it "same els" $ do
+             groupElems [1,2,2,2,4,5,5] `shouldBe` [[1],[2,2,2],[4],[5,5]]
